087e1c4ff6b7cb733e75d38ae6ae1d57b9950565
Bob <937131090@qq.com>
2016-04-22 15:15:59 +0800

    ZXB
    123

---
+++ IT04Bob/IT04Bob/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IT04Bob
+//
+//  Created by Mac on 16/4/22.
+//  Copyright © 2016年 张喜波. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IT04Bob/IT04Bob/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IT04Bob
+//
+//  Created by Mac on 16/4/22.
+//  Copyright © 2016年 张喜波. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
